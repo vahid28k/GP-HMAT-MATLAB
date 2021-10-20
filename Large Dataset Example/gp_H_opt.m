7674d205b7bb9f210a5167b9432eea4f6aff1e04
vkeshava <34436184+vahid28k@users.noreply.github.com>
2021-10-19 18:58:41 -0600

    Add files via upload

--- Large Dataset Example/gp_H_opt.m
+++ Large Dataset Example/gp_H_opt.m
@@ -1,14 +1,15 @@
 function [f,gvec] = gp_H_opt(nodes,y,params,l)
 global liter fiter cc;
+dy=size(y,2);
 
 warning('off');
 params{1}(2)={l};
 n=size(nodes,2);
 [xdet,xdet_d,energy,energy_d,~,~]=lkl_eval(nodes,{y},params);
-f=-0.5*xdet-0.5*sum(energy,'all')-0.5*n*log(2*pi);
+f=-0.5*dy*xdet-0.5*trace(energy)-0.5*n*log(2*pi);
 gvec=zeros(1,length(l));
 for i=1:length(l)
-    gvec(i)=-0.5*xdet_d{i}-0.5*sum(energy_d{i},'all');
+    gvec(i)=-0.5*dy*xdet_d{i}-0.5*trace(energy_d{i});
 end
 f=-f;
 gvec=-gvec;
